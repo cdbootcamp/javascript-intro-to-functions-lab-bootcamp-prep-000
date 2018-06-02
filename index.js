ecd424b9d24a065232925012476df58e1cc15abb
code23nr@gmail.com <code23nr@gmail.com>
2018-06-02 01:44:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   return "I can't hear you!"
 }
+
+function sayHiToGrandma(string) {
+  if string === uppercase
+  return "YES INDEED!"
+}
